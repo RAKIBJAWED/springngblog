e69814f21b49ca43a71d5954ffe9b5691c5291bc
Rakib Jawed <Rakib@DESKTOP-TOTPJLD>
2021-07-24 20:23:40 +0530

    Initial commit

---
+++ springngblog/src/test/java/com/rakib/jawed/springngblog/SpringngblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.jawed.springngblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringngblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
